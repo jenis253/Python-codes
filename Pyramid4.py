4dd9c044ca71d696ada24ceeacaf5b2ddf80f4ea
jenish gajera <ayurvisionenterprise07@gmail.com>
2025-08-25 09:03:03 +0530

    Update Pyramid4.py

--- Pyramid4.py
+++ Pyramid4.py
@@ -4,5 +4,6 @@ for i in range(1,n+1):
     for k in range(n-i):
             print(" " ,end=" ")
     for j in range(1,i+1):
-        print("* ", end=" ")   
+       print("* ", end=" ")   
+
     print()
